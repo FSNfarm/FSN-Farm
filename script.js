a9b10fec2bfe3a0b358801e7442207e9c2b17aac
FSNfarm <uangbuatgila@gmail.com>
2026-09-25 21:45:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -366,3 +366,11 @@ document
 showCurrentDate();
 
 loadFarmData();
+const hamburger = document.getElementById("hamburger");
+const sidebar = document.getElementById("sidebar");
+
+if (hamburger && sidebar) {
+  hamburger.addEventListener("click", function () {
+    sidebar.classList.toggle("open");
+  });
+}
