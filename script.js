49aed36ac47d9fec6ea3e78b94715c774a4c07eb
FSNfarm <uangbuatgila@gmail.com>
2026-09-26 19:57:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // SUPABASE CONFIGURATION
 // ================================
 
-const SUPABASE_URL = "https://wdxhbnffddbjvljqwdsn.supabase.co;
+const SUPABASE_URL = "https://wdxhbnffddbjvljqwdsn.supabase.co";
 const SUPABASE_KEY = "sb_publishable_0QshMmZk2xHqjOndQxKEnw_eigTr_QC";
 
 const supabaseClient = window.supabase.createClient(
